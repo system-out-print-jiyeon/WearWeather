03e982a0acc12c2da7d49136eff22e64703753c0
Jiyeon <jy971114@naver.com>
2023-02-05 23:36:30 +0900

    [관리자] 사용자 조회, 사용자 등록

--- src/main/java/kr/co/weather/admin/service/AdminService.java
+++ src/main/java/kr/co/weather/admin/service/AdminService.java
@@ -1,9 +1,13 @@
 package main.java.kr.co.weather.admin.service;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class AdminService {
@@ -40,28 +44,57 @@ public class AdminService {
 	
 	private void selectUser() throws IOException {
 		System.out.println("=======> 사용자  조회");
+		try {
+			File userFile = new File("C:\\storage\\User.txt"); 
+			// 현재 등록된 사용자 읽어오기
+	        FileReader filereader = new FileReader(userFile);
+	        BufferedReader bufReader = new BufferedReader(filereader);
+	        String line = "";
+	        while((line = bufReader.readLine()) != null){
+	            System.out.println(line);
+	        }
+	        bufReader.close();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
 
 	}
 	
 	private void insertUser() throws IOException {
 		System.out.println("=======> 사용자  등록");
 		File userFile = new File("C:\\storage\\User.txt"); 
+		List<String> currentUser = new ArrayList<>();
+		
 		userFile.createNewFile();
 		
 		Scanner sc = new Scanner(System.in);
 		System.out.println("등록하실 사용자 아이디를 입력하세요.");
 		String userName = sc.next();
 		
-		BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(userFile));
-		
-		if(userFile.isFile() && userFile.canWrite()){
-            //쓰기
-            bufferedWriter.write(userName);
-            //개행문자쓰기
-            bufferedWriter.newLine();
-            
-            bufferedWriter.close();
+		// 현재 등록된 사용자 읽어오기
+        FileReader filereader = new FileReader(userFile);
+        BufferedReader bufReader = new BufferedReader(filereader);
+        String line = "";
+        while((line = bufReader.readLine()) != null){
+            currentUser.add(line);
         }
+        //.readLine()은 끝에 개행문자를 읽지 않는다.            
+        bufReader.close();
+
+        // 등록된 사용자 아닐경우에만 등록
+        // append=true 기존값에 이어쓰기
+		BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(userFile, true));
+		if(!currentUser.contains(userName)) {
+			if(userFile.isFile() && userFile.canWrite()){
+	            bufferedWriter.write(userName);
+	            bufferedWriter.newLine();
+	            bufferedWriter.close();
+	            System.out.println("[" + userName + "] 사용자 등록완료.");
+	        }
+		}else {
+			System.out.println("이미 등록된 사용자입니다.");
+			this.insertUser();
+		}
 		
 
 		
